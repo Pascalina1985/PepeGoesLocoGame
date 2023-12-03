232edb98bb052fa93eab0e09efe00c5af78e10bc
Pascal Megnin <136910002+Pascalina1985@users.noreply.github.com>
2023-12-03 08:46:29 +0100

    added new Backgroundobjects

--- models/world.class.js
+++ models/world.class.js
@@ -14,14 +14,28 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0), //nur x Koordinate
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/air.png', 720),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720)
+        new BackgroundObject('img/5_background/layers/air.png', 719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2), //nur x Koordinate
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3)
     ];
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d'); //nur ctx kann auf canvas gemalt werden
