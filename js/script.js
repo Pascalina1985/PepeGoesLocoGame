7bd4bd20401917ca0fff38c3d76a12b18c7a6cc1
Pascal Megnin <136910002+Pascalina1985@users.noreply.github.com>
2024-01-02 13:44:49 +0100

    added fullScreenMode

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function fullscreen() {
+    openFullscreen();
+}
+
+function openFullscreen() {
+    let fullscreenDiv = document.getElementById('fullscreen');
+    let canvas = fullscreenDiv.querySelector('canvas');
+
+    canvas.width = 2560;
+    canvas.height = 480;
+
+    if (fullscreenDiv.requestFullscreen) {
+        fullscreenDiv.requestFullscreen();
+    } else if (fullscreenDiv.webkitRequestFullscreen) { /* Safari */
+        fullscreenDiv.webkitRequestFullscreen();
+    } else if (fullscreenDiv.msRequestFullscreen) { /* IE11 */
+        fullscreenDiv.msRequestFullscreen();
+    }
+}
+
+
+
+function closeFullscreen() {
+    let fullscreenDiv = document.getElementById('fullscreen');
+    let canvas = fullscreenDiv.querySelector('canvas');
+
+    canvas.width = 720;
+    canvas.height = 480;
+
+    if (fullscreenDiv.exitFullscreen) {
+        fullscreenDiv.exitFullscreen();
+    } else if (fullscreenDiv.webkitExitFullscreen) { /* Safari */
+        fullscreenDiv.webkitExitFullscreen();
+    } else if (fullscreenDiv.msExitFullscreen) { /* IE11 */
+        fullscreenDiv.msExitFullscreen();
+    }
+}
